c6d8869bff2b4632a507d9a218e311205cf6a5b0
Wambuilucy <wambuilucie99@gmail.com>
2020-10-11 19:04:42 +0300

    Bulk commit-no specific chnage

--- funn/settings.py
+++ funn/settings.py
@@ -77,8 +77,10 @@ WSGI_APPLICATION = 'funn.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'lucy',
+        'USER': '<Your Database Username >',
+    'PASSWORD':'<Your Database Password>',
     }
 }
 
