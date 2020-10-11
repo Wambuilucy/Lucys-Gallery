3f1c57b0c16795d1634163138d5a8976d143f9c5
Wambuilucy <wambuilucie99@gmail.com>
2020-10-11 19:40:48 +0300

    Bulk commit of changes

--- funn/urls.py
+++ funn/urls.py
@@ -13,14 +13,18 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.conf.urls import url, include
+
 from django.contrib import admin
+from django.conf.urls import include, url
+from django.conf import settings
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
+    url(r'^admin/', include('admin.site.urls')),
     url(r'^gallery/',include('gallery.urls'))
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    
 
+# urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
