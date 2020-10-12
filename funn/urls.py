7f9a2180fb5995e86e97b03c511f81e97535e5d2
Wambuilucy <wambuilucie99@gmail.com>
2020-10-12 08:18:40 +0300

    made some changes in the urls.py

--- funn/urls.py
+++ funn/urls.py
@@ -15,13 +15,17 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.conf.urls import include, url
+from django.conf.urls import url,include
 from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^gallery/',include('gallery.urls'))
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 # urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     
