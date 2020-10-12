d2b454d73e7fa3b92025fabe4c773c7d6a816c25
Wambuilucy <wambuilucie99@gmail.com>
2020-10-12 08:12:50 +0300

    changes in my urls.py

--- gallery/urls.py
+++ gallery/urls.py
@@ -17,8 +17,13 @@ from django.contrib import admin
 from django.urls import path
 from django.conf.urls import url, include
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^$', views.index, name='index'),
+    url(r'^gallery', views.gallery, name='gallery'),
+    url(r'^search/', views.search_category, name = 'search_category')
     #url('^$',views.welcome,name = 'welcome'),
 ]
